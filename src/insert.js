47636ec82974a9961ff9cadb76d9b78d47765a5c
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2022-11-17 16:28:57 +0100

    fix insert.js

--- src/insert.js
+++ src/insert.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { Greet } from "./index.js";
 
- class Insert extends React.Component {
-  
+class Insert extends React.Component {
   constructor(props) {
     super(props);
 
-
-    this.state = { name: 'Julia' };
+    this.state = { name: "Julia" };
     this.changeName = this.changeName.bind(this);
   }
 
-    changeName(newName) {
-  this.setState({
-    name: newName
-  });
+  changeName(newName) {
+    this.setState({
+      name: newName,
+    });
+  }
+
+  render() {
+    return <Greet name={this.state.name} onChange={this.changeName} />;
+  }
 }
-  }
-  
-   render() {
-    return <Greet name={this.state.name} onChange={this.changeName} />
-  }
-ReactDOM.render(
-	<Insert />,
-	document.getElementById('root')
-);
+
+ReactDOM.render(<Insert />, document.getElementById("root"));
