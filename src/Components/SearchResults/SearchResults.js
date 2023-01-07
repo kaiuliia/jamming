b17bfeb57a5bcdd8d3d499138148c0e292444cdd
kaiuliia <76730155+kaiuliia@users.noreply.github.com>
2023-01-07 22:32:48 +0100

    created hook for  SearchRes component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -4,26 +4,25 @@
 import React from "react";
 import { Track } from "../Track/Track";
 import { TrackList } from "../Tracklist/TrackList";
+import React, { useState } from "react";
 import "./SearchResults.css";
 
-export class SearchResults extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export function SearchResults (props) {
 
-  render() { 
+
+  
     return (
       <div className="SearchResults">
         <h2>Results</h2>
         <TrackList
-          tracks={this.props.searResults}
-          addTrack={this.props.onAdd}
-          onRemove={this.props.onRemove}
+          tracks={props.searResults}
+          addTrack={props.onAdd}
+          onRemove={props.onRemove}
           isRemoval={true}
         />
       </div>
     );
-  }
+  
 }
  
 // props: searchResult
